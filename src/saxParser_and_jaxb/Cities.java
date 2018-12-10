5b9940113bc6b81b2ddce1898a666eb4974507dd
Volodymyr Kryvobok <krivobok@ukraina.local>
2018-12-10 15:55:15 +0200

    added home work 010 SAX and JAXB parsers

---
+++ src/saxParser_and_jaxb/Cities.java
@@ -0,0 +1,40 @@
+package saxParser_and_jaxb;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Cities {
+ 
+	private String city;
+	private String street;
+	private int numberOfHouse;
+
+	public String getCity() {
+		return city;
+	}
+
+	@XmlElement
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	@XmlElement
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public int getNumberOfHouse() {
+		return numberOfHouse;
+	}
+
+	@XmlElement
+	public void setNumberOfHouse(int numberOfHouse) {
+		this.numberOfHouse = numberOfHouse;
+	}
+}
+
